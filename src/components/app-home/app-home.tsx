f4bef5fbfa240c6627186c1879a554d87795e729
Marcel Canhisares <marcel@canhisares.com>
2019-07-02 15:52:31 -0400

    initial commit

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,42 @@
+import { Component, h } from '@stencil/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css'
+})
+export class AppHome {
+
+  render() {
+    return [
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>Home</ion-title>
+        </ion-toolbar>
+      </ion-header>,
+
+      <ion-content class="ion-padding">
+        <p>
+          Welcome to the PWA Toolkit. You can use this starter to build entire
+          apps with web components using Stencil and ionic/core! Check out the
+          README for everything that comes in this starter out of the box and
+          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+        </p>
+
+        <ion-button onClick={_ => { 
+          this.camera() 
+        }}>Profile page</ion-button>
+      </ion-content>
+    ];
+  }
+
+  async camera() {
+    const ph = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    console.log(ph);
+  }
+}
---
+++ src/global/app.ts
@@ -0,0 +1,11 @@
+import '@ionic/core';
+import '@ionic/pwa-elements';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+defineCustomElements(window);
+
+// import { setupConfig } from '@ionic/core';
+
+// setupConfig({
+//   mode: 'ios'
+// });
