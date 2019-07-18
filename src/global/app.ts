087161308c4bd906ef4dc62d0b90323859098f43
Julio César <jcesarmobile@gmail.com>
2019-07-18 20:08:15 +0200

    remove defineCustomElements

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,8 +1,5 @@
 import '@ionic/core';
 import '@ionic/pwa-elements';
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-defineCustomElements(window);
 
 // import { setupConfig } from '@ionic/core';
 
